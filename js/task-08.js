81728038688aa92b3836588aeb00e3c17ab9e1c1
Rostyslav-Yefremov <rostislavefremow@gmail.com>
2022-01-18 23:46:43 +0200

    Add 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formProperties = event.currentTarget.elements;
+
+  const formData = {
+    email: formProperties.email.value,
+    password: formProperties.password.value,
+  };
+
+  if (formData.email === '' || formData.password === '') {
+    return alert('Пожалуйста, заполните все поля! Я вас очень прошу.');
+  }
+
+  console.log(formData);
+  event.currentTarget.reset();
+});
