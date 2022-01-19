48fe62eb4941261bf4fc03ed0a83c14f88174a28
Rostyslav-Yefremov <rostislavefremow@gmail.com>
2022-01-19 10:54:36 +0200

    Add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector('.change-color');
+const colorRef = document.querySelector('.color');
+
+buttonRef.addEventListener('click', () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorRef.textContent = color;
+});
